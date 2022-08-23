fbc18f440974a79d7e0c3bd586941ed4809813c6
Andrii Pozniak <andriipozniak@gmail.com>
2022-08-23 19:19:15 +0300

    change task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,12 @@
 const btn = document.querySelector(`.change-color`);
 const bodyEl = document.querySelector(`body`);
+const pageEl = document.querySelector(`span`);
 
 btn.addEventListener(`click`, getRandomHexColor);
 
 function getRandomHexColor(event) {
-  bodyEl.style.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-
+  bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  pageEl.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`)
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
